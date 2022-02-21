8b8739685f5455a60e9ab8b82f56fc114f514974
munyua <98756995+munyuawilson@users.noreply.github.com>
2022-02-21 09:56:43 +0000

    Add files via upload

---
+++ Karu.py
@@ -0,0 +1,19 @@
+#scrap the notice board of karu
+
+from bs4 import BeautifulSoup
+import requests
+import lxml
+url="https://karu.ac.ke/student-notice-board/"
+get=requests.get(url).text
+content=BeautifulSoup(get,'html.parser')
+All_div=content.find('div',class_="gdlr-core-pbf-column gdlr-core-column-60 gdlr-core-column-first")
+
+ntitles=All_div.find_all('h3',class_="gdlr-core-blog-title gdlr-core-skin-title")
+for n in ntitles:
+    notices=n.a.text.strip()
+    
+    with open('notice.txt',"a") as file:
+        file.write(notices)
+        file.close()
+    print(notices)
+        
